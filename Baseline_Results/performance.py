81ab022502e8811d85279eec3e6e6d9fc6f5140f
Elena Kochkina <ekochkina@turing.ac.uk>
2019-02-22 19:19:00 +0000

    Add files via upload

---
+++ Baseline_Results/performance.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Get the performance numbers
+
+"""
+import json
+from sklearn.metrics import f1_score, accuracy_score, confusion_matrix, precision_recall_fscore_support
+
+#%%
+submission_file = "answer.json" # this file contains output of branchLSTM model for Tasks A and B
+reference_file = "final-eval-key.json"
+
+
+with open(submission_file, 'r') as f:
+    resultLSTM = json.load(f)
+    
+with open(reference_file, 'r') as f:
+    labels = json.load(f)
+
+
+#%%
+def convert_stance(labelint):
+    convertdict = {}
+    convertdict[0] = 'support'
+    convertdict[1] = 'comment'
+    convertdict[2] = 'deny'
+    convertdict[3] = 'query'
+    return convertdict[labelint]
+
+def convert_veracity(labelint):
+    convertdict = {}
+    convertdict[0] = 'true'
+    convertdict[1] = 'false'
+    convertdict[2] = 'unverified'
+
+    return convertdict[labelint]
+
+#%%
+
+task  = "subtaskaenglish"    
+task_results = resultLSTM[task]
+task_labels  = labels[task]
+
+true = []
+pred = []
+for k in task_labels.keys():
+    
+    true.append(task_labels[k])
+    pred.append(task_results[k])
+
+print (task)
+print ("Macro F score ", f1_score(true,pred, average = 'macro'))
+print ("Accuracy ",accuracy_score(true,pred))
+print ("Confusion matrix \n",confusion_matrix(true,pred, labels=['support','comment','deny','query']))
+print ("Precision, recall, fscore, support per class \n",precision_recall_fscore_support(true,pred, labels=['support','comment','deny','query']))
+
+
+#%%
+
+task  = "subtaskbenglish"    
+task_results = resultLSTM[task]
+task_labels  = labels[task]
+
+true = []
+pred = []
+for k in task_labels.keys():
+    
+    true.append(task_labels[k])
+    pred.append(task_results[k][0])
+
+print (task)
+print ("Macro F score ", f1_score(true,pred, average = 'macro'))
+print ("Accuracy ",accuracy_score(true,pred))
+print ("Confusion matrix \n",confusion_matrix(true,pred, labels=['true','false','unverified']))
+print ("Precision, recall, fscore, support per class \n",precision_recall_fscore_support(true,pred, labels=['true','false','unverified']))
+
