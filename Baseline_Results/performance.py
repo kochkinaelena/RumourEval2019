4f3e2fdc53d1456f461b744bb687f012101d20c0
Elena Kochkina <ekochkina@turing.ac.uk>
2019-02-22 19:47:39 +0000

    Update performance.py

--- Baseline_Results/performance.py
+++ Baseline_Results/performance.py
@@ -9,11 +9,15 @@ from sklearn.metrics import f1_score, accuracy_score, confusion_matrix, precisio
 
 #%%
 submission_file = "answer.json" # this file contains output of branchLSTM model for Tasks A and B
+submission_fileB = "answerB_myNileTMRG.json"
 reference_file = "final-eval-key.json"
 
 
 with open(submission_file, 'r') as f:
     resultLSTM = json.load(f)
+    
+with open(submission_fileB, 'r') as f:
+    resultNile = json.load(f)
     
 with open(reference_file, 'r') as f:
     labels = json.load(f)
@@ -67,11 +71,44 @@ pred = []
 for k in task_labels.keys():
     
     true.append(task_labels[k])
-    pred.append(task_results[k][0])
+#    pred.append(task_results[k][0]) # if we simply do that, we won't get the same performance as in competition scorer
+    
+    yhat, confidence = task_results[k]
+    if confidence>=0.5:
+        pred.append(yhat)
+    else:
+        pred.append('unverified')
+    
 
 print (task)
 print ("Macro F score ", f1_score(true,pred, average = 'macro'))
-print ("Accuracy ",accuracy_score(true,pred))
 print ("Confusion matrix \n",confusion_matrix(true,pred, labels=['true','false','unverified']))
 print ("Precision, recall, fscore, support per class \n",precision_recall_fscore_support(true,pred, labels=['true','false','unverified']))
+
+#%%
+
+task  = "subtaskbenglish"    
+task_results = resultNile[task]
+task_labels  = labels[task]
+
+true = []
+pred = []
+for k in task_labels.keys():
+    
+    true.append(task_labels[k])
+#    pred.append(task_results[k][0])
+    
+    yhat, confidence = task_results[k]
+    if confidence>=0.5:
+        pred.append(yhat)
+    else:
+        pred.append('unverified')
+
+print (task)
+print ("Macro F score ", f1_score(true,pred, average = 'macro'))
+print ("Confusion matrix \n",confusion_matrix(true,pred, labels=['true','false','unverified']))
+print ("Precision, recall, fscore, support per class \n",precision_recall_fscore_support(true,pred, labels=['true','false','unverified']))
+
+
+
 
